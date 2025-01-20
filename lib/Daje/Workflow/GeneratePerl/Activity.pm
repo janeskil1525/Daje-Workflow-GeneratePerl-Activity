c1478cbf410c0bdf0a16a9797459576b31af4806
janeskil1525 <janeskil1525@gmail.com>
2025-01-20 17:39:59 +0100

    First Commit

--- lib/Daje/Workflow/GeneratePerl/Activity.pm
+++ lib/Daje/Workflow/GeneratePerl/Activity.pm
@@ -1,7 +1,36 @@
 package Daje::Workflow::GeneratePerl::Activity;
-use 5.008001;
-use strict;
-use warnings;
+use Mojo::Base 'Daje::Workflow::Common::Activity::Base', -base, -signatures;
+
+#
+# NAME
+# ====
+#
+# Daje::Workflow::GeneratePerl::Activity - It creates perl code
+#
+# SYNOPSIS
+# ========
+#
+#     use Daje::Workflow::GeneratePerl::Activity;
+#
+# DESCRIPTION
+# ===========
+#
+# Daje::Workflow::GeneratePerl::Activity is ...
+#
+# LICENSE
+# =======
+#
+# Copyright (C) janeskil1525.
+#
+# This library is free software; you can redistribute it and/or modify
+# it under the same terms as Perl itself.
+#
+# AUTHOR
+# ======
+#
+# janeskil1525 E<lt>janeskil1525@gmail.comE<gt>
+#
+
 
 our $VERSION = "0.01";
 
@@ -10,30 +39,4 @@ our $VERSION = "0.01";
 1;
 __END__
 
-=encoding utf-8
-
-=head1 NAME
-
-Daje::Workflow::GeneratePerl::Activity - It's new $module
-
-=head1 SYNOPSIS
-
-    use Daje::Workflow::GeneratePerl::Activity;
-
-=head1 DESCRIPTION
-
-Daje::Workflow::GeneratePerl::Activity is ...
-
-=head1 LICENSE
-
-Copyright (C) janeskil1525.
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=head1 AUTHOR
-
-janeskil1525 E<lt>janeskil1525@gmail.comE<gt>
-
-=cut
 
