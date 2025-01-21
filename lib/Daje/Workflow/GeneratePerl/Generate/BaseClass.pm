0922bd70964c244c26cc8f90d57f2d0bff79b535
janeskil1525 <janeskil1525@gmail.com>
2025-01-21 18:44:23 +0100

    First Commit

--- lib/Daje/Workflow/GeneratePerl/Generate/BaseClass.pm
+++ lib/Daje/Workflow/GeneratePerl/Generate/BaseClass.pm
@@ -1,31 +1,31 @@
-use v5.40;
-use feature 'class';
-no warnings 'experimental::class';
+package Daje::Workflow::GeneratePerl::Generate::BaseClass;
+use Mojo::Base 'Daje::Workflow::GeneratePerl::Base::Common' ,-base, -signatures;
+
 
 our $VERSION = '0.01';
 
-class Daje::Generate::Perl::Generate::BaseClass {
-    field $config :param;
-    field $template :param;
 
-    method generate() {
-        my $tpl = $template->get_data_section('baseclass');
+has 'config' ;
+has 'template';
 
-        my $base_name_space = $config->{CLASS}->{base_name_space};
+sub generate() {
+    my $tpl = $template->get_data_section('baseclass');
 
-        my $date = localtime();
-        $tpl =~ s/<<date>>/$date/ig;
-        $tpl =~ s/<<base_name_space>>/$base_name_space/ig;
+    my $base_name_space = $config->{CLASS}->{base_name_space};
 
-        my $output = Daje::Generate::Output::Perl::Class->new(
-            config         => $config,
-            table_name     => "Base",
-            perl           => $tpl,
-            name_space_dir => "base_space_dir",
-        );
-        $output->save_file();
-    }
+    my $date = localtime();
+    $tpl =~ s/<<date>>/$date/ig;
+    $tpl =~ s/<<base_name_space>>/$base_name_space/ig;
+
+    my $output = Daje::Generate::Output::Perl::Class->new(
+        config         => $config,
+        table_name     => "Base",
+        perl           => $tpl,
+        name_space_dir => "base_space_dir",
+    );
+    $output->save_file();
 }
+
 
 
 1;
--- lib/Daje/Workflow/GeneratePerl/Generate/Methods.pm
+++ lib/Daje/Workflow/GeneratePerl/Generate/Methods.pm
@@ -1,89 +1,90 @@
-use v5.40;
-use feature 'class';
-no warnings 'experimental::class';
+package Daje::Generate::Perl::Generate::Methods;
+use Mojo::Base 'Daje::Workflow::GeneratePerl::Base::Common' ,-base, -signatures;
+
+
+
 
 our $VERSION = '0.01';
 
-class Daje::Generate::Perl::Generate::Methods :isa(Daje::Generate::Perl::Base::Common) {
-    field $fields :param :reader;
-    field $pkey :reader;
-    field $fkey :reader;
-    field $insert :reader;
-    field $update :reader;
-    field $select_fields :reader;
-    field $select_method :reader;
+has 'fields' ;
+has 'pkey' ;
+has 'fkey' ;
+has 'insert' ;
+has 'update' ;
+has 'select_fields' ;
+has 'select_method' ;
 
-    method generate() {
-        $pkey = $self->_get_from_pkey();
-        $fkey = $self->_get_from_fkey();
-        $insert = $self->_insert_method();
-        $update = $self->_update_method();
-        $select_fields = $self->_select_fields();
-        $select_method = $self->_select_method();
-    }
-
-    method _select_method() {
-        my $tpl = $self->template->get_data_section('load_list');
-
-        return $tpl;
-    }
-
-    method _select_fields() {
-        my $tpl = $self->template->get_data_section('fields_method');
-        my $select = $self->fields->select();
-        my $p_key = $self->fields->primary_key();
-        my $table_name = $self->json->{table_name};
-
-        $tpl =~ s/<<select_fields>>/$select/ig;
-        $tpl =~ s/<<primary_key>>/$p_key/ig;
-        $tpl =~ s/<<table_name>>/$table_name/ig;
-        return $tpl;
-    }
-
-    method _update_method() {
-        my $tpl = $self->template->get_data_section('update_data');
-        my $table_name = $self->json->{table_name};
-        $tpl =~ s/<<table_name>>/$table_name/ig;
-        return $tpl;
-    }
-
-    method _insert_method() {
-        my $tpl = $self->template->get_data_section('insert_data');
-        my $table_name = $self->json->{table_name};
-        $tpl =~ s/<<table_name>>/$table_name/ig;
-        return $tpl;
-    }
-
-    method _get_from_fkey() {
-        my $load_from_fkey = "";
-        my $f_key = $fields->foreign_keys();
-        my $table_name = $self->json->{table_name};
-        my $select = $fields->select();
-        if (defined $f_key) {
-            my $length = scalar @{$f_key};
-            for (my $i = 0; $i < $length; $i++) {
-                my $tpl = $self->template->get_data_section('load_from_fkey');
-                $tpl =~ s/<<foreign_key>>/@{$f_key}[$i]/ig;
-                $tpl =~ s/<<table_name>>/$table_name/ig;
-                $tpl =~ s/<<select_fields>>/$select/ig;
-                $load_from_fkey .= $tpl . "\n\n";
-            }
-        }
-        return $load_from_fkey;
-    }
-
-    method _get_from_pkey(){
-        my $load_from_pkey = $self->template->get_data_section('load_from_pkey');
-        my $p_key = $self->fields->primary_key();
-        my $select = $self->fields->select();
-
-        my $table_name = $self->json->{table_name};
-        $load_from_pkey =~ s/<<primary_key>>/$p_key/ig;
-        $load_from_pkey =~ s/<<table_name>>/$table_name/ig;
-        $load_from_pkey =~ s/<<select_fields>>/$select/ig;
-        return $load_from_pkey;
-    }
+sub generate($self) {
+    $pkey = $self->_get_from_pkey();
+    $fkey = $self->_get_from_fkey();
+    $insert = $self->_insert_method();
+    $update = $self->_update_method();
+    $select_fields = $self->_select_fields();
+    $select_method = $self->_select_method();
 }
+
+sub _select_method($self) {
+    my $tpl = $self->template->get_data_section('load_list');
+
+    return $tpl;
+}
+
+sub _select_fields($self) {
+    my $tpl = $self->template->get_data_section('fields_method');
+    my $select = $self->fields->select();
+    my $p_key = $self->fields->primary_key();
+    my $table_name = $self->json->{table_name};
+
+    $tpl =~ s/<<select_fields>>/$select/ig;
+    $tpl =~ s/<<primary_key>>/$p_key/ig;
+    $tpl =~ s/<<table_name>>/$table_name/ig;
+    return $tpl;
+}
+
+sub _update_method($self) {
+    my $tpl = $self->templates->get_data_section('update_data');
+    my $table_name = $self->json->{table_name};
+    $tpl =~ s/<<table_name>>/$table_name/ig;
+    return $tpl;
+}
+
+sub _insert_method($self) {
+    my $tpl = $self->templates->get_data_section('insert_data');
+    my $table_name = $self->json->{table_name};
+    $tpl =~ s/<<table_name>>/$table_name/ig;
+    return $tpl;
+}
+
+sub _get_from_fkey($self) {
+    my $load_from_fkey = "";
+    my $f_key = $fields->foreign_keys();
+    my $table_name = $self->json->{table_name};
+    my $select = $fields->select();
+    if (defined $f_key) {
+        my $length = scalar @{$f_key};
+        for (my $i = 0; $i < $length; $i++) {
+            my $tpl = $self->template->get_data_section('load_from_fkey');
+            $tpl =~ s/<<foreign_key>>/@{$f_key}[$i]/ig;
+            $tpl =~ s/<<table_name>>/$table_name/ig;
+            $tpl =~ s/<<select_fields>>/$select/ig;
+            $load_from_fkey .= $tpl . "\n\n";
+        }
+    }
+    return $load_from_fkey;
+}
+
+sub _get_from_pkey($self){
+    my $load_from_pkey = $self->templates->get_data_section('load_from_pkey');
+    my $p_key = $self->fields->primary_key();
+    my $select = $self->fields->select();
+
+    my $table_name = $self->json->{table_name};
+    $load_from_pkey =~ s/<<primary_key>>/$p_key/ig;
+    $load_from_pkey =~ s/<<table_name>>/$table_name/ig;
+    $load_from_pkey =~ s/<<select_fields>>/$select/ig;
+    return $load_from_pkey;
+}
+
 1;
 #################### pod generated by Pod::Autopod - keep this line to make pod updates possible ####################
 
